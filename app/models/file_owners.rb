d5320cc0adaecb774f525a12bad492754592234b
vagfot <vagfot@yahoo.com>
2017-12-03 23:20:45 +0200

    Comments

--- app/models/file_owners.rb
+++ app/models/file_owners.rb
@@ -1,5 +1,10 @@
 module FileOwners
-
+  # Implement a group_by_owners function that:
+  #
+  #                                          Accepts a hash containing the file owner name for each file name.
+  # Returns a hash containing an array of file names for each owner name, in any order.
+  # For example, for hash {'Input.txt' => 'Randy', 'Code.py' => 'Stan', 'Output.txt' => 'Randy'}
+  # the group_by_owners function should return {'Randy' => ['Input.txt', 'Output.txt'], 'Stan' => ['Code.py']}.
   def self.group_by_owners(files)
     owners = {}
     files.each do |key, value|
